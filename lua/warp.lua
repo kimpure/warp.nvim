3220bdac82f02531e1fd99451e838d52b6e55eef
kimpure <gimleni593@gmail.com>
2025-11-09 11:04:12 +0900

    Fix: update logic

--- lua/warp.lua
+++ lua/warp.lua
@@ -61,10 +61,10 @@ function module.warp_visual(opts)
 
 	if start_row > end_row or (start_row == end_row and start_col > end_col) then
         shift_line(start_row, end_col - 1, open_keyword)
-        shift_line(end_row, start_col + 1, close_keyword)
+        shift_line(end_row, start_col + 1 + #open_keyword, close_keyword)
     else
         shift_line(start_row, start_col, open_keyword)
-        shift_line(end_row, end_col + 1, close_keyword)
+        shift_line(end_row, end_col + #open_keyword, close_keyword)
     end
 
 
